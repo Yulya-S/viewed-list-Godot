a3ccd5c2a11d7985b713fcc9c65f082493eb8203
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-06-11 23:46:16 +0800

    Section filtering

--- viewed-list/scripts/sections.gd
+++ viewed-list/scripts/sections.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 @onready var SectionContainer = $ScrollContainer/VBoxContainer
+@onready var FilterName = $Filters/Name
+@onready var FilterPageName = $Filters/PartName
+@onready var FilterChapterName = $Filters/ChapterName
+@onready var FilterCount = $Filters/Count
+@onready var FilterOrder = $Filters/Order
 
 var section = load("res://scenes/section_box.tscn")
 
@@ -8,8 +13,7 @@ var section = load("res://scenes/section_box.tscn")
 # Создание страницы
 func _ready() -> void:
 	$Head.SectionTItles.text = "К тайтлам"
-	#add_sections("SELECT * FROM sections;")
-	add_sections("SELECT s.*, j.titles_count FROM `sections` AS s INNER JOIN (SELECT t.section_id, COUNT(t.section_id) AS titles_count FROM `titles` AS t GROUP BY t.section_id) AS j ON j.section_id = s.id;")
+	add_sections("SELECT s.*, j.titles_count FROM `sections` AS s INNER JOIN (SELECT t.section_id, COUNT(t.section_id) AS titles_count FROM `titles` AS t GROUP BY t.section_id) AS j ON j.section_id = s.id ORDER BY s.title;")
 
 
 # Заполнение страницы разделами
@@ -21,3 +25,35 @@ func add_sections(request_text: String):
 	for i in Global.db.query_result:
 		SectionContainer.add_child(section.instantiate())
 		SectionContainer.get_child(-1).set_section(i)
+
+
+# Изменение значения Количества тайтлов
+func _on_count_text_changed() -> void: Global.valide_text(FilterCount)
+
+# Нажатие кнопки фильтра
+func _on_filter_button_down() -> void:
+	# Фильтры
+	var filter_text: String = ""
+	if FilterName.get_text() != "":
+		filter_text = Global.filter_text(filter_text, "s.title", FilterName.get_text(), "LIKE")
+	if FilterPageName.get_text() != "":
+		filter_text = Global.filter_text(filter_text, "s.part_name", FilterPageName.get_text(), "LIKE")
+	if FilterChapterName.get_text() != "":
+		filter_text = Global.filter_text(filter_text, "s.chapter_name", FilterChapterName.get_text(), "LIKE")
+	if FilterCount.get_text() != "":
+		filter_text = Global.filter_text(filter_text, "j.titles_count", FilterCount.get_text(), ">=")
+	if filter_text != "": filter_text = " WHERE " + filter_text	
+	
+	# Сортировка
+	var order: String = ""
+	match FilterOrder.selected:
+		0: order = "s.id"
+		1: order = "s.title"
+		2: order = "j.titles_count DESC"
+		_: order = "s.id"
+	
+	# Сборка запроса
+	var request: String = "SELECT s.*, j.titles_count FROM `sections` AS s INNER JOIN " + \
+						  "(SELECT t.section_id, COUNT(t.section_id) AS titles_count FROM `titles` AS t GROUP BY t.section_id) " + \
+						  "AS j ON j.section_id = s.id " + filter_text + " ORDER BY " + order + ";"
+	add_sections(request)
