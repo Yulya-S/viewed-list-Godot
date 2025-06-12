bca26ac0e600b2b34350a0be2cc5363b4b1e9951
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-06-13 06:17:06 +0800

    Added display of errors and number of titles related to the section

--- viewed-list/scripts/section_page.gd
+++ viewed-list/scripts/section_page.gd
@@ -28,6 +28,17 @@ func set_section(new_section) -> void:
 	ChapterName.set_text(value.chapter_name)
 	_on_display_toggled(bool(value.display))
 	$Window/Delete.visible = true
+	$Window/TitlesCount.visible = true
+
+
+# Отображение ошибки если тайтл уже есть в разделе
+func check_section():
+	Global.db.query('SELECT id FROM `sections` WHERE title = "' + Name.get_text() + '";')
+	var value = Global.db.query_result
+	Error.visible = false
+	if not section and len(value) > 0: Error.visible = true
+	else: for i in value: if i.id != section.id: Error.visible = true
+	if Error.visible: Error.set_text("раздел с таким именем уже существует")
 	
 	
 # Изменение названия раздела
@@ -36,6 +47,9 @@ func _on_name_text_changed() -> void:
 	if len(text) > 0 and "\n" in text:
 		Name.set_text(Name.get_text().replace("\n", ""))
 		Name.release_focus()
+
+func _on_name_text_set() -> void: check_section()
+
 
 # Изменение названия Части
 func _on_part_name_text_changed() -> void:
@@ -63,6 +77,10 @@ func _on_display_toggled(toggled_on: bool) -> void:
 
 # Обработка нажатия кнопки создания / изменения раздела
 func _on_apply_button_down() -> void:
+	check_section()
+	if Name.get_text() == "":
+		Error.visible = true
+		Error.set_text("Поле названия должно быть не пустым")
 	if Error.visible: return
 	if section:
 		Global.db.query('UPDATE `sections` SET title = "' + Name.get_text() + \
@@ -79,12 +97,14 @@ func _on_apply_button_down() -> void:
 func _on_delete_button_down() -> void:
 	var save_id: int = section.id
 	
+	# Удаление всех тайтлов которые относятся к удаляемому разделу
 	Global.db.query("Select id FROM `titles` WHERE section_id = " + str(section.id) + ";")
 	for i in Global.db.query_result:
 		Global.db.query("DELETE FROM `titles` WHERE id = " + str(i.id) + ";")
 		Global.db.query('UPDATE `sqlite_sequence` SET seq = seq - 1 WHERE name = "titles";')
 		Global.db.query("UPDATE `titles` SET id = id - 1 WHERE id > " + str(i.id) + ";")
 	
+	# Удаление самого раздела
 	Global.db.query("DELETE FROM `sections` WHERE id = " + str(section.id) + ";")
 	Global.db.query('UPDATE `sqlite_sequence` SET seq = seq - 1 WHERE name = "sections";')
 	Global.db.query("UPDATE `sections` SET id = id - 1 WHERE id > " + str(save_id) + ";")
