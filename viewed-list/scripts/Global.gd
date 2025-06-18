d80f7541306ed5a7e0396b50090c79a40d201112
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-06-18 20:42:49 +0800

    Removed filter_text function

--- viewed-list/scripts/Global.gd
+++ viewed-list/scripts/Global.gd
@@ -11,10 +11,3 @@ func valide_text(text_container) -> void:
 		var filtered_text = ""
 		for i in text: if i.is_valid_int(): filtered_text += i
 		if filtered_text != text: text_container.set_text(filtered_text)	
-
-# Сборка текста для фильтра
-func filter_text(text: String, column: String, value: String, sep: String = "=") -> String:
-	if text != "": text += " AND "
-	if sep == "LIKE": value = '"%' + value + '%"'
-	text += column + " " + sep + " " + value
-	return text
