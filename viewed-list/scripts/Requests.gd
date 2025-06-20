dd17beda7e5ce7326de1fb7ce75bd4b6d763f0e1
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-06-21 02:39:06 +0800

    loading old db

--- viewed-list/scripts/Requests.gd
+++ viewed-list/scripts/Requests.gd
@@ -2,13 +2,20 @@ extends Node
 enum Tables {USERS, SECTIONS, TITLES, SQLITE_SEQUENCE} # Таблицы в базе данных
 var db: SQLite = null # Подключенная база данных
 
-# открытие базы данных
+# Открытие базы данных
 func _ready() -> void: if not db: connecting_users_db()
+
+# Генерация названия базы данных
+func generate_db_name() -> String:
+	var base_name: String = ""
+	const chars: String = 'abcdefghijklmnopqrstuvwxyz1234567890'
+	for i in range(10): base_name += chars[randi()%len(chars)]
+	return base_name
 
 # Создание таблицы пользователей
 func create_users_db() -> void:
 	db.query("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,
-		login VARCHAR(255), password VARCHAR(255), base VARCHAR(255), display BOOLEAN);""")
+		login VARCHAR(255), password VARCHAR(255), base VARCHAR(255), color_scheme INT, order_by BOOLEAN);""")
 
 func create_title_db() -> void:
 	db.query("""CREATE TABLE IF NOT EXISTS sections (id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -124,3 +131,38 @@ func select_titles(filters: String = "", order: String = "") -> Array:
 		"FROM `titles` t INNER JOIN ( SELECT s.* FROM `sections` s) j ON "+\
 		"j.id = t.section_id"+filters+order+";")
 	return db.query_result
+
+# Получение данных всех данных из таблицы
+func _get_data_from_table(table: Tables) -> Array:
+	Requests.db.query("SELECT * FROM `"+_get_table_name(table)+"`;")
+	return Requests.db.query_result
+
+# Загрузка данных из старой базы данных
+func select_old_db(old_db: String):
+	var existing_users: Array = []
+	for i in select(Tables.USERS, "*"): existing_users.append(i.login)
+	Requests.connecting_db(old_db)
+	var users: Array = _get_data_from_table(Tables.USERS)
+	var sections: Dictionary = {}
+	for i in _get_data_from_table(Tables.SECTIONS): sections[i.id] = i
+	var titles: Array = _get_data_from_table(Tables.TITLES)
+	# Заполнение таблицы пользователей
+	for i in users:
+		if i.nickname in existing_users: continue
+		connecting_users_db()
+		var sections_ids: Array = []
+		var password: String = ""
+		for l in i.password: password += char(l)
+		var base_name: String = Requests.generate_db_name()
+		insert_record(Tables.USERS, ['"'+i.nickname+'"', '"'+password+'"', '"'+Marshalls.utf8_to_base64(base_name)+'"', 0, i.order_by])
+		connecting_db("res://bases/"+base_name)
+		for l in titles: if i.id == l.user_id:
+			# Заполнение таблицы разделов
+			if l.section_id not in sections_ids:
+				sections_ids.append(l.section_id)
+				var data: Dictionary = sections[l.section_id]
+				insert_record(Tables.SECTIONS, ['"'+data.title+'"', '"'+data.bloc_name+'"', '"'+data.chapter_name+'"', data.display])
+			# Заполнение таблицы тайтлов
+			insert_record(Tables.TITLES, [sections_ids.find(l.section_id)+1, "'"+l.title+"'",
+				l.status+1, l.bloc, l.chapter, '"'+l.note+'"', l.stars+1])				
+	connecting_users_db()
--- viewed-list/scripts/pages/registration.gd
+++ viewed-list/scripts/pages/registration.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var Password = $Password
 @onready var Show = $Password/Show
 @onready var Remember = $Remember
+@onready var Background = $Background
 
 # Автоматический вход в программу
 func _process(_delta: float) -> void: if Global.config.enter: _entrance(Global.config.login, Global.config.password)
@@ -19,14 +20,11 @@ func _on_show_toggled(_toggled_on: bool) -> void:
 	if _toggled_on: Password.add_theme_color_override("font_color", Color.html("#ffffff"))
 	else: Password.add_theme_color_override("font_color", Color.html("#00000000"))
 
-# Нажатие кнопки создания нового пользоавтеля
+# Обработка нажатия кнопки создания нового пользоавтеля
 func _on_registration_button_down() -> void:
-	var base_name: String = ""
-	const chars: String = 'abcdefghijklmnopqrstuvwxyz1234567890'
-	for i in range(10): base_name += chars[randi()%len(chars)]
 	Requests.insert_record(Requests.Tables.USERS, ['"'+Login.get_text()+'"',
 		'"'+Marshalls.utf8_to_base64(Password.get_text())+'"',
-		'"'+Marshalls.utf8_to_base64(base_name)+'"', 0])
+		'"'+Marshalls.utf8_to_base64(Requests.generate_db_name())+'"', 0])
 	_on_enter_button_down()
 
 # Вход в программу
@@ -36,9 +34,22 @@ func _entrance(user_login: String, user_password: String) -> void:
 		Requests.connecting_db("res://bases/"+Marshalls.base64_to_utf8(users[0].base)+".db")
 		Global.emit_signal("change_program_mod", Global.ProgramModes.TITLE)
 	
-# Нажатие кнопки входа в программу
+# Обработка нажатия кнопки входа в программу
 func _on_enter_button_down() -> void:
 	if Remember.button_pressed:
 		Global.config = {"login"=Login.get_text(), "password"=Marshalls.utf8_to_base64(Password.get_text()), "enter"=true}
 		Global.update_config()
 	_entrance(Login.get_text(), Marshalls.utf8_to_base64(Password.get_text()))
+
+# Обработка нажатия кнопки загрузки старой базы данных
+func _on_load_button_down() -> void:
+	Background.visible = true
+	Background.get_child(0).visible = true
+
+# Закрытие окна выбора файла
+func _on_file_dialog_canceled() -> void: Background.visible = false
+
+# Считыванние данных из старой базы
+func _on_file_dialog_file_selected(path: String) -> void:
+	Requests.select_old_db(path)
+	_on_file_dialog_canceled()
