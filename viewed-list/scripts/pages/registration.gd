2f483f0f8b24f947f735880c0a642f4050a9869c
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-06-20 16:51:43 +0800

    toggle password visibility

--- viewed-list/scripts/pages/registration.gd
+++ viewed-list/scripts/pages/registration.gd
@@ -2,17 +2,16 @@ extends Control
 # Подключение путей к объектам в сцене
 @onready var Login = $Login
 @onready var Password = $Password
+@onready var Show = $Password/Show
 @onready var Remember = $Remember
-
-var password_text: String = "" # Текст пароля
 
 # Изменение значения логина
 func _on_login_text_changed() -> void: Global.text_changed_TextEdit(Login)
 
 # Изменение значения пароля
-func _on_password_text_changed() -> void:
-	var caret = Password.get_caret_column()
-	password_text = Password.get_text()
-	for i in range(len(Password.get_text())): Password.text[i] = "*"
-	Password.set_caret_column(caret)
-	Global.text_changed_TextEdit(Password)
+func _on_password_text_changed() -> void: Global.text_changed_TextEdit(Password)
+
+# Переключение видимости пароля
+func _on_show_toggled(_toggled_on: bool) -> void:
+	if _toggled_on: Password.add_theme_color_override("font_color", Color.html("#ffffff"))
+	else: Password.add_theme_color_override("font_color", Color.html("#00000000"))
