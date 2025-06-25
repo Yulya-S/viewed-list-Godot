b66defcd6c94a41d3a347a4870aa0215e809d061
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-06-26 00:30:13 +0800

    Getting indexes of titles suitable for filtering

--- viewed-list/scripts/pages/random.gd
+++ viewed-list/scripts/pages/random.gd
@@ -7,8 +7,12 @@ extends Node2D
 @onready var FiltersRating = $Filters/Rating
 @onready var Background = $Background
 
+var titles: Array = [] # Индексты тайтлов которые могут быть получены
+
 # Создание страницы
 func _ready() -> void:
+	_on_filter_button_down()
+	# Скрытие полей
 	Head.Add.visible = false
 	# Замена цвета
 	FilterBox.color = ColorScheme.get_color(Global.Colors.COLOR2)
@@ -16,3 +20,12 @@ func _ready() -> void:
 	for i in [FiltersSection, FiltersStatus, FiltersRating]:
 		ColorScheme.set_font_color(i.get_child(-1))
 	for i in range(3): ColorScheme.set_font_color(Background.get_child(i))
+
+# Обработка нажатия кнопки фильтрации
+func _on_filter_button_down() -> void:
+	var filter_text: String = Requests.add_part_request("", "section", FiltersSection.selected)
+	filter_text = Requests.add_part_request(filter_text, "status", FiltersStatus.selected)
+	filter_text = Requests.add_part_request(filter_text, "rating", FiltersRating.get_text())
+	titles = []
+	for i in Requests.select(Requests.Tables.TITLES, "id", filter_text): titles.append(i.id)
+	print(titles)
