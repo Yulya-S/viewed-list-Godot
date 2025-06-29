a2f16bd0596fd2510e3c12b254519c9acdaf9f15
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-06-30 03:39:16 +0800

    Update main.gd

--- viewed-list/scripts/main.gd
+++ viewed-list/scripts/main.gd
@@ -3,7 +3,6 @@ extends Node2D
 const WindowsDir: String = "res://scenes/windows/"
 const PagesDir: String = "res://scenes/pages/"
 
-
 # Подключение сигналов
 func _ready() -> void:
 	Global.connect("open_object_page", Callable(self, "_open_object_page"))
