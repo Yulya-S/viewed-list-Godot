13588a16375bf7ec287622a4343cbbe9765eb095
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-06-30 01:43:33 +0800

    Update head.gd

--- viewed-list/scripts/fragments/head.gd
+++ viewed-list/scripts/fragments/head.gd
@@ -25,7 +25,7 @@ func _on_sections_titles_button_down() -> void: Global.emit_signal("change_progr
 # Обработка нажатия кнопки выхода из аккаунта
 func _on_exit_button_down() -> void:
 	Requests.connecting_users_db()
-	Global.config = {"login"="", "password"="", "enter"=false}
+	Global.config = {"enter"=false}
 	Global.update_config()
 	Global.emit_signal("change_program_mod", Global.ProgramModes.REGISTRATION)
 
